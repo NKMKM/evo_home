a8523bf5e9df713b6367a3c42a34b197d0ac3c44
slizis228 <benritov@gmail.com>
2025-07-23 23:21:16 +0300

    big changes in slider

--- frontend/src/components/mainpage_components/Discount.jsx
+++ frontend/src/components/mainpage_components/Discount.jsx
@@ -9,31 +9,32 @@ const Discount = () => {
         window.location.href = url
     }
     const {t} = useTranslation('slider/Discount')
+    const selectedLang = localStorage.getItem('language') || 'en';	
     return (
-    <div className="flex flex-col  justify-around w-full min-h-[600px]  md:min-h-[800px] discount-bg-image bg-cover">
+    <div className="flex flex-col  justify-around w-full min-h-[480px]   md:min-h-[800px] discount-bg-image bg-cover">
       {/* Header */}
-      <div className="flex flex-row h-[90px] md:h-[200px] lg:bg-black/55 pt-4 md:pt-0">
+      <div className="flex flex-row h-[80px] md:h-[200px] lg:bg-black/55 pt-4 md:pt-0">
 
         {/* Header text */}
         <div className="relative bg-black/55 lg:bg-transparent flex-1 md:w-[75%] z-0">
           {/* Жёлтая полоска — ПОД текстом, НО НАД фоном */}
-          <div className="bg-[#f9b33b] h-4 md:h-10 w-[90%] mx-auto absolute bottom-4 lg:bottom-6 md:bottom-8 left-0 right-0 md:left-[10px] z-[100]"></div>
+          <div className="bg-[#f9b33b] h-4 md:h-10 w-[90%] mx-auto absolute bottom-2 lg:bottom-6 md:bottom-8 left-0 right-0 md:left-[10px] z-[100]"></div>
 
           {/* Текст — самый верхний */}
-          <h1 className=" md:bg-transparent text-white text-center font-bold text-[34px] sm:text-2xl md:text-[140px] absolute inset-0 flex items-center justify-center z-[200] text-shadow-lg">
+          <h1 className={`md:bg-transparent text-white text-center font-bold ${selectedLang === 'ru' ? 'text-[44px]' : 'text-[36px]'}  sm:text-2xl md:text-[140px] absolute inset-0 flex items-center justify-center z-[200] text-shadow-lg`}>
             {t('h')}
           </h1>
         </div>
 
         {/* Days */}
-        <div className="flex flex-col items-center justify-center bg-[#ffb429] text-black w-[40%] md:w-[25%] p-4 md:p-5">
+        <div className="flex flex-col items-center justify-center bg-[#ffb429] text-black w-[40%] md:w-[25%] p-4 md:p-5 lg:space-y-5 lg:mr-[45px]">
           <p className="text-2xl sm:text-5xl md:text-7xl font-light">{t('left_block.p')}</p>
           <strong className="text-3xl sm:text-6xl md:text-7xl font-bold">{t('left_block.strong')}</strong>
         </div>
       </div>
 
       {/* Comment */}
-      <div className="flex items-center bg-black/58 text-white text-2xl sm:text-xl md:text-[29px] font-light px-4 py-6 md:px-32 md:py-0 md:h-[130px]">
+      <div className="flex items-center bg-black/58 text-white text-2xl sm:text-xl md:text-[29px] font-[200] px-4 py-6 md:px-32 md:py-0 md:h-[130px]">
         <p className="text-center md:text-left">{t('comment.p')}<span className='text-[#f9b33b]'>{t('comment.span')}</span></p>
       </div>
 
@@ -44,9 +45,9 @@ const Discount = () => {
         <Link className="flex justify-center items-center 
             h-14 md:h-16 lg:h-20 
             w-full md:w-[300px] lg:w-[360px] 
-            bg-[#f9b33b] font-bold 
+            bg-[#f9b33b] font-medium 
             text-xl md:text-3xl lg:text-2xl 
-            rounded-lg hover:scale-110 transition duration-300" to={'/contacts'}>
+             hover:scale-110 transition duration-300" to={'/contacts'}>
             {t('button')}
         </Link>
           <div className=" justify-center gap-4 hidden lg:flex lg:flex-row">
@@ -60,9 +61,9 @@ const Discount = () => {
         </div>
 
         {/* Conditions */}
-        <div className="md:w-[500px] bg-black/55  md:border-6 lg:border-[#f9b33b] lg:rounded-lg text-white px-4 md:px-5 md:py-9 lg:backdrop-blur-sm">
+        <div className="md:w-[500px] bg-black/55  md:border-6 lg:border-[#f9b33b] lg:rounded-lg text-white px-4 md:px-5 md:py-7 lg:backdrop-blur-sm">
           <p className="font-semibold text-xl md:text-3xl hidden lg:inline">{t('condition.h')}</p>
-          <ul className=" text-center font-[300] lg:font-[500] lg:text-start pl-6 pt-2 md:pt-4 text-base md:text-[22px]">
+          <ul className=" text-center font-[200] lg:font-[500] lg:text-start pl-6 pt-2 md:pt-4 text-base md:text-[21px]">
             <li>{t('condition.li1')} <span className='text-[#ffb429]'>{t('condition.span1')}</span></li>
             <li>{t('condition.li2')} <span className='text-[#ffb429]'>{t('condition.span2')}</span></li>
             <li>{t('condition.li3')} <span className='text-[#ffb429]'>{t('condition.span3')}</span> {t('condition.li4')}</li>
